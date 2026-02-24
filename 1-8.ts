e559d537f7bbabcc4fec84c60a1f5b87a1aadf25
unknown <jaym991776@gmail.com>
2026-02-24 16:10:38 -0500

    1-8: Type annotation in Class

---
+++ 1-8.ts
@@ -0,0 +1,54 @@
+//this.name을 사용하고 싶다면 미리 name 타입지정을 해줘야하낟.
+//Class의 return타입지정? 항상 object복사라서 굳이 할 필요 없다
+//하지만 Construcor parameter 타입지정은 권장
+//Rest Parameter, Default Parameter 등 가능
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHi(nickName: string): string {
+    console.log("Hi, I am", this.name);
+    return `Call me ${nickName}`;
+  }
+}
+
+let person1 = new Person("Adam", 18);
+person1.sayHi("Ad");
+
+class Car {
+  model: string;
+  price: number;
+  constructor(model: string, price: number) {
+    this.model = model;
+    this.price = price;
+  }
+  tax(): number {
+    let tax = this.price * 0.1;
+    console.log(tax);
+    return tax;
+  }
+}
+let car1 = new Car("Sonata", 3000);
+car1.tax();
+
+class Word {
+  num: number[];
+  str: string[];
+  constructor(...params: (number | string)[]) {
+    this.num = [];
+    this.str = [];
+    params.forEach((e) => {
+      if (typeof e === "string") {
+        this.str.push(e);
+      } else {
+        this.num.push(e);
+      }
+    });
+  }
+}
+let word1 = new Word("HI", 150);
+console.log(word1.num);
+console.log(word1.str);
